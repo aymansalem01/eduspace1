2ddddea97f54e9a1f96c69d141b9109cbf9c7017
Loay-Ghanem <liuyghanem901@gmail.com>
2024-08-20 21:23:34 +0300

    [add] frontend

--- resources/views/aboutus.blade.php
+++
@@ -1,9 +0,0 @@
-<title>edu space aboutus</title>
-@extends('layout')
-@section('contant')
-<div class="content">
-    <div class="mid-section">
-        <h1> about us page <h1>
-    </div>
-</div>
-@endsection
